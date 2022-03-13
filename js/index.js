7b5ffad936eb3a1befe7b8dc05929e4ccc0ef0d3
Mariane Felix <marianefelix59@gmail.com>
2022-03-13 16:02:43 -0300

    :sparkles: remove funções não utilizadas

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-const statOptionsElements = document.querySelectorAll('a');
-
-let statsList = [];
-
-const previousStatsTexts = {
-    'daily': 'Yesterday - ',
-    'weekly': 'Last Week - ',
-    'monthly': 'Last month - '
-}
-
 const clearPreviousLink = () => {
     const activeLink = document.getElementsByClassName('active')[0];
 
@@ -21,106 +11,32 @@ const getActiveLink = () => {
 }
 
 const setDailyOption = () => {
-    const windowLocationHash = getActiveLink();
-
-    if (windowLocationHash == "" || windowLocationHash === "#daily") {
-        const dailyOption = document.getElementById('daily');
-        dailyOption.click();
-    }
+    const dailyOption = document.getElementById('daily');
+    dailyOption.click();
 }
 
-const addEventListenerToLinks = (statsData) => {
-    statsList = statsData;
+const addEventListenerToLinks = (statsList) => {
+    const statOptionsElements = document.querySelectorAll('a');
 
     statOptionsElements.forEach((linkElement) => {
         linkElement.addEventListener('click', () => {
             clearPreviousLink();
             linkElement.setAttribute('class', 'active');
 
-            renderStats();
+            renderStats(statsList);
         })
-    })
+    });
 
     setDailyOption();
 }
 
-const createStatCard = (id) => {
-    let statCardElement = document.createElement('div');
-
-    statCardElement.setAttribute('class', 'stats card');
-    statCardElement.setAttribute('id', id);
-
-    return statCardElement;
+const previousStatsTexts = {
+    'daily': 'Yesterday - ',
+    'weekly': 'Last Week - ',
+    'monthly': 'Last month - '
 }
 
-const createTimeframeCard = () => {
-    let timeframeCard = document.createElement('div');
-
-    timeframeCard.setAttribute('class', 'timeframe card');
-
-    return timeframeCard;
-}
-
-const createTimeframeHeader = (title) => {
-    const headerElement = document.createElement('div');
-    const titleElement = document.createElement('p');
-    const buttonElement = document.createElement('button');
-    const imgElement = document.createElement('img');
-
-    titleElement.setAttribute('class', 'title');
-    titleElement.textContent = title;
-
-    imgElement.setAttribute('src', 'images/icon-ellipsis.svg');
-    imgElement.setAttribute('alt', 'Mais opções');
-
-    buttonElement.setAttribute('type', 'button');
-    buttonElement.appendChild(imgElement);
-
-    headerElement.appendChild(titleElement);
-    headerElement.appendChild(buttonElement);
-
-    return headerElement;
-}
-
-const createStatDataElement = (previousData, currentData) => {
-    const divElement = document.createElement('div');
-    const currentElement = document.createElement('h2');
-    const previousElement = document.createElement('p');
-
-    currentElement.setAttribute('class', 'current');
-    currentElement.textContent = currentData;
-
-    previousElement.setAttribute('class', 'previous');
-    previousElement.textContent = previousData;
-
-    divElement.appendChild(currentElement);
-    divElement.appendChild(previousElement);
-
-    return divElement;
-}
-
-const createStatsElements = (mainElement) => {
-    const activeLink = getActiveLink().replace('#', '');
-    const statOption = activeLink !== '' ? activeLink : 'daily';
-
-    statsList.forEach((stat) => {
-        const cardId = stat.title.toLowerCase().replace(' ', '-');
-        const previous = previousStatsTexts[statOption] + stat.timeframes[statOption].previous + 'hrs';
-        const current = stat.timeframes[statOption].current + 'hrs';
-
-        const cardElement = createStatCard(cardId);
-        const timeframeCard = cardElement.appendChild(createTimeframeCard());
-
-        const headerElement = createTimeframeHeader(stat.title);
-        const statDataElement = createStatDataElement(previous, current);
-
-        timeframeCard.appendChild(headerElement);
-        timeframeCard.appendChild(statDataElement);
-        mainElement.appendChild(cardElement);
-    });
-}
-
-const updateStatsElements = (activeLink) => {
+const updateStatsInfo = (activeLink, statsList) => {
     statsList.forEach((stat, index) => {
         const previous = 
             previousStatsTexts[activeLink] + 
@@ -135,16 +51,10 @@ const updateStatsElements = (activeLink) => {
     });
 }
 
-const renderStats = () => {
-    const mainElement = document.getElementsByTagName('main')[0];
-    const statsElements = document.getElementsByClassName('stats');
+const renderStats = (statsList) => {
     const activeLink = document.getElementsByClassName('active')[0].id;
 
-    if (statsElements.length > 0) {
-        updateStatsElements(activeLink);
-    } else {
-        createStatsElements(mainElement);
-    }
+    updateStatsInfo(activeLink, statsList);
 }
 
 const fetchData = async () => {
